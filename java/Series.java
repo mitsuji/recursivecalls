d38c69a35e51a1758faf1f696c07bd24917b8635
mitsuji <tkms@mitsuji.org>
2022-08-16 10:26:24 +0900

    ok

---
+++ java/Series.java
@@ -0,0 +1,16 @@
+
+public class Series {
+
+    public static void main (String [] args) {
+        System.out.println(String.format("%d",series(100000)));
+    }
+
+	// java.lang.StackOverflowError
+    private static long series(long x) {
+		if (x==0)
+			return 0;
+		else
+			return x + series(x-1);
+    }
+
+}
